ea26c877f89e0c6d4e22d1b2e01098c32286dd49
Ian Fisher <iafisher@fastmail.com>
2021-07-31 12:35:30 -0400

    Lex symbols

--- src/lexer.go
+++ src/lexer.go
@@ -22,6 +22,8 @@ func NewLexer(program string) *Lexer {
 }
 
 func (l *Lexer) NextToken() *Token {
+	l.skipWhitespace()
+
 	loc := l.copyLocation()
 	if l.index >= len(l.program) {
 		return &Token{Type: TOKEN_EOF, Value: "", Loc: loc}
@@ -32,15 +34,33 @@ func (l *Lexer) NextToken() *Token {
 	case isDigit(ch):
 		value := l.readInteger()
 		return &Token{Type: TOKEN_INT, Value: value, Loc: loc}
+	case isSymbolFirstCharacter(ch):
+		value := l.readSymbol()
+		return &Token{Type: TOKEN_SYMBOL, Value: value, Loc: loc}
 	default:
 		l.advance()
 		return &Token{Type: TOKEN_UNKNOWN, Value: string(ch), Loc: loc}
 	}
 }
 
+func (l *Lexer) skipWhitespace() {
+	for l.index < len(l.program) && isWhitespace(l.program[l.index]) {
+		l.advance()
+	}
+}
+
 func (l *Lexer) readInteger() string {
 	start := l.index
 	for l.index < len(l.program) && isDigit(l.program[l.index]) {
+		l.advance()
+	}
+	return l.program[start:l.index]
+}
+
+func (l *Lexer) readSymbol() string {
+	start := l.index
+	l.advance()
+	for l.index < len(l.program) && isSymbolCharacter(l.program[l.index]) {
 		l.advance()
 	}
 	return l.program[start:l.index]
@@ -66,8 +86,21 @@ func isDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9'
 }
 
+func isSymbolFirstCharacter(ch byte) bool {
+	return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch < +'Z') || (ch == '_')
+}
+
+func isSymbolCharacter(ch byte) bool {
+	return isSymbolFirstCharacter(ch) || isDigit(ch)
+}
+
+func isWhitespace(ch byte) bool {
+	return ch == ' ' || ch == '\n' || ch == '\t' || ch == '\v' || ch == '\f' || ch == '\r'
+}
+
 const (
 	TOKEN_INT     = "TOKEN_INT"
 	TOKEN_EOF     = "TOKEN_EOF"
+	TOKEN_SYMBOL  = "TOKEN_SYMBOL"
 	TOKEN_UNKNOWN = "TOKEN_UNKNOWN"
 )
--- src/lexer_test.go
+++ src/lexer_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestNextToken(t *testing.T) {
+func TestNextTokenWithIntegerLiterals(t *testing.T) {
 	input := "100"
 	lexer := NewLexer(input)
 
@@ -13,12 +13,35 @@ func TestNextToken(t *testing.T) {
 	checkToken(t, token, TOKEN_EOF, "", 1, 4)
 }
 
+func TestNextTokenWithSymbols(t *testing.T) {
+	input := "a b c abc a1_0"
+	lexer := NewLexer(input)
+
+	token := lexer.NextToken()
+	checkToken(t, token, TOKEN_SYMBOL, "a", 1, 1)
+
+	token = lexer.NextToken()
+	checkToken(t, token, TOKEN_SYMBOL, "b", 1, 3)
+
+	token = lexer.NextToken()
+	checkToken(t, token, TOKEN_SYMBOL, "c", 1, 5)
+
+	token = lexer.NextToken()
+	checkToken(t, token, TOKEN_SYMBOL, "abc", 1, 7)
+
+	token = lexer.NextToken()
+	checkToken(t, token, TOKEN_SYMBOL, "a1_0", 1, 11)
+
+	token = lexer.NextToken()
+	checkToken(t, token, TOKEN_EOF, "", 1, 15)
+}
+
 func checkToken(t *testing.T, token *Token, ttype string, value string, line int, column int) {
 	if token.Type != ttype {
-		t.Fatalf("Wrong token type: got %q, expected %q", token.Type, ttype)
+		t.Fatalf("Wrong token type (line %d, col %d): got %q, expected %q", token.Loc.Line, token.Loc.Column, token.Type, ttype)
 	}
 	if token.Value != value {
-		t.Fatalf("Wrong token value: got %q, expected %q", token.Value, value)
+		t.Fatalf("Wrong token value (line %d, col %d): got %q, expected %q", token.Loc.Line, token.Loc.Column, token.Value, value)
 	}
 	if token.Loc.Line != line {
 		t.Fatalf("Wrong line: got %d, expected %d", token.Loc.Line, line)
