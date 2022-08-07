18901b06b726526fff9591fede64ebbff92ee893
Ian Fisher <ian@iafisher.com>
2022-08-07 08:46:48 -0400

    Start work on lexer

--- src/common.rs
+++ src/common.rs
@@ -1,5 +1,28 @@
+use std::fmt;
+
+#[derive(Clone, Debug)]
 pub struct Location {
-    file: String,
-    column: u32,
-    line: u32,
+    pub file: String,
+    pub column: u32,
+    pub line: u32,
+}
+
+impl Location {
+    pub fn empty() -> Self {
+        Location {
+            file: String::new(),
+            column: 0,
+            line: 0,
+        }
+    }
+}
+
+impl fmt::Display for Location {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "line {}, column {} of {}",
+            self.line, self.column, self.file
+        )
+    }
 }
--- src/lexer.rs
+++ src/lexer.rs
@@ -1,14 +1,169 @@
 use super::common;
 
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum TokenType {
+    // Literals
+    Integer,
+    // Operators
+    Plus,
+    // Miscellaneous
     EOF,
     Unknown,
 }
 
+#[derive(Clone, Debug)]
 pub struct Token {
     type_: TokenType,
     value: String,
     location: common::Location,
 }
 
-pub struct Lexer {}
+impl Token {
+    pub fn new(type_: TokenType, value: &str, location: common::Location) -> Self {
+        Token {
+            type_: type_,
+            value: String::from(value),
+            location: common::Location::empty(),
+        }
+    }
+
+    pub fn without_location(type_: TokenType, value: &str) -> Self {
+        Token::new(type_, value, common::Location::empty())
+    }
+}
+
+impl PartialEq for Token {
+    fn eq(&self, other: &Self) -> bool {
+        self.type_ == other.type_ && self.value == other.value
+    }
+}
+impl Eq for Token {}
+
+pub struct Lexer {
+    program: String,
+    index: usize,
+    start: usize,
+    location: common::Location,
+    start_location: common::Location,
+    token: Token,
+}
+
+impl Lexer {
+    pub fn new(file: &str, program: &str) -> Self {
+        let location = common::Location {
+            file: String::from(file),
+            column: 1,
+            line: 1,
+        };
+        let mut lexer = Lexer {
+            program: String::from(program),
+            index: 0,
+            start: 0,
+            location: location.clone(),
+            start_location: location.clone(),
+            token: Token {
+                type_: TokenType::Unknown,
+                value: String::from(""),
+                location: location,
+            },
+        };
+        // "Prime the pump" so that we can immediately call token() to retrieve the
+        // first token.
+        lexer.next();
+        lexer
+    }
+
+    pub fn token(&self) -> Token {
+        self.token.clone()
+    }
+
+    pub fn next(&mut self) {
+        self.skip_whitespace();
+
+        if self.index >= self.program.len() {
+            self.make_token(TokenType::EOF);
+            return;
+        }
+
+        self.start = self.index;
+        self.start_location = self.location.clone();
+
+        let c = self.ch();
+        if c == '+' {
+            self.increment_index();
+            self.make_token(TokenType::Plus)
+        } else if c.is_numeric() {
+            self.read_number();
+        } else {
+            self.make_token(TokenType::Unknown);
+        }
+    }
+
+    fn read_number(&mut self) {
+        while self.index < self.program.len() && self.ch().is_numeric() {
+            self.increment_index();
+        }
+        self.make_token(TokenType::Integer)
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.index < self.program.len() && self.ch().is_whitespace() {
+            self.increment_index();
+        }
+    }
+
+    fn make_token(&mut self, type_: TokenType) {
+        let token = Token::new(
+            type_,
+            &self.program[self.start..self.index],
+            self.start_location.clone(),
+        );
+        self.start = self.index;
+        self.start_location = self.location.clone();
+        self.token = token;
+    }
+
+    fn increment_index(&mut self) {
+        if self.index >= self.program.len() {
+            return;
+        }
+
+        if self.ch() == '\n' {
+            self.location.column = 1;
+            self.location.line += 1;
+        } else {
+            self.location.column += 1;
+        }
+        self.index += 1;
+    }
+
+    fn ch(&self) -> char {
+        // TODO: more efficient way to do this?
+        self.program.chars().nth(self.index).unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_expressions() {
+        let mut lexer = Lexer::new("<string>", "1 + 1");
+        assert_eq!(
+            lexer.token(),
+            Token::without_location(TokenType::Integer, "1")
+        );
+        lexer.next();
+        assert_eq!(lexer.token(), Token::without_location(TokenType::Plus, "+"),);
+        lexer.next();
+        assert_eq!(
+            lexer.token(),
+            Token::without_location(TokenType::Integer, "1")
+        );
+        lexer.next();
+        assert_eq!(lexer.token(), Token::without_location(TokenType::EOF, ""));
+        lexer.next();
+        assert_eq!(lexer.token(), Token::without_location(TokenType::EOF, ""));
+    }
+}
