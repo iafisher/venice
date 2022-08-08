290928b04ac0b1275f02e795a5f4cd5c0726b9fd
Ian Fisher <ian@iafisher.com>
2022-08-08 08:24:26 -0400

    More work on lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,11 +1,32 @@
 use super::common;
+use std::collections::HashMap;
 
-#[derive(Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum TokenType {
     // Literals
     Integer,
+    Symbol,
     // Operators
+    Minus,
+    Percent,
     Plus,
+    Slash,
+    Star,
+    // Keywords
+    And,
+    Assert,
+    Const,
+    Else,
+    For,
+    Func,
+    If,
+    In,
+    Let,
+    New,
+    Not,
+    Or,
+    Record,
+    While,
     // Miscellaneous
     EOF,
     Unknown,
@@ -26,10 +47,6 @@ impl Token {
             location: common::Location::empty(),
         }
     }
-
-    pub fn without_location(type_: TokenType, value: &str) -> Self {
-        Token::new(type_, value, common::Location::empty())
-    }
 }
 
 impl PartialEq for Token {
@@ -46,6 +63,36 @@ pub struct Lexer {
     location: common::Location,
     start_location: common::Location,
     token: Token,
+}
+
+lazy_static! {
+    static ref ONE_CHAR_TOKENS: HashMap<char, TokenType> = {
+        let mut m = HashMap::new();
+        m.insert('-', TokenType::Minus);
+        m.insert('%', TokenType::Percent);
+        m.insert('+', TokenType::Plus);
+        m.insert('/', TokenType::Slash);
+        m.insert('*', TokenType::Star);
+        m
+    };
+    static ref KEYWORDS: HashMap<&'static str, TokenType> = {
+        let mut m = HashMap::new();
+        m.insert("and", TokenType::And);
+        m.insert("assert", TokenType::Assert);
+        m.insert("const", TokenType::Const);
+        m.insert("else", TokenType::Else);
+        m.insert("for", TokenType::For);
+        m.insert("func", TokenType::Func);
+        m.insert("if", TokenType::If);
+        m.insert("in", TokenType::In);
+        m.insert("let", TokenType::Let);
+        m.insert("new", TokenType::New);
+        m.insert("not", TokenType::Not);
+        m.insert("or", TokenType::Or);
+        m.insert("record", TokenType::Record);
+        m.insert("while", TokenType::While);
+        m
+    };
 }
 
 impl Lexer {
@@ -91,11 +138,13 @@ impl Lexer {
         self.start_location = self.location.clone();
 
         let c = self.ch();
-        if c == '+' {
+        if let Some(type_) = ONE_CHAR_TOKENS.get(&c) {
             self.increment_index();
-            self.make_token(TokenType::Plus)
-        } else if c.is_numeric() {
+            self.make_token(*type_)
+        } else if c.is_ascii_digit() {
             self.read_number()
+        } else if is_symbol_first_character(c) {
+            self.read_symbol()
         } else {
             self.make_token(TokenType::Unknown)
         }
@@ -106,6 +155,18 @@ impl Lexer {
             self.increment_index();
         }
         self.make_token(TokenType::Integer)
+    }
+
+    fn read_symbol(&mut self) -> Token {
+        while self.index < self.program.len() && is_symbol_character(self.ch()) {
+            self.increment_index()
+        }
+        let value = &self.program[self.start..self.index];
+        if let Some(type_) = KEYWORDS.get(value) {
+            self.make_token(*type_)
+        } else {
+            self.make_token(TokenType::Symbol)
+        }
     }
 
     fn skip_whitespace(&mut self) {
@@ -146,26 +207,73 @@ impl Lexer {
     }
 }
 
+fn is_symbol_first_character(c: char) -> bool {
+    c.is_ascii_alphabetic() || c == '_'
+}
+
+fn is_symbol_character(c: char) -> bool {
+    c.is_ascii_alphanumeric() || c == '_'
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn token(type_: TokenType, value: &str) -> Token {
+        Token::new(type_, value, common::Location::empty())
+    }
+
     #[test]
     fn simple_expressions() {
         let mut lexer = Lexer::new("<string>", "1 + 1");
-        assert_eq!(
-            lexer.token(),
-            Token::without_location(TokenType::Integer, "1")
-        );
-        assert_eq!(lexer.next(), Token::without_location(TokenType::Plus, "+"),);
-        assert_eq!(
-            lexer.next(),
-            Token::without_location(TokenType::Integer, "1")
-        );
-        assert_eq!(lexer.next(), Token::without_location(TokenType::EOF, ""));
+        assert_eq!(lexer.token(), token(TokenType::Integer, "1"));
+        assert_eq!(lexer.next(), token(TokenType::Plus, "+"),);
+        assert_eq!(lexer.next(), token(TokenType::Integer, "1"));
+        assert_eq!(lexer.next(), token(TokenType::EOF, ""));
 
         // Make sure that multiple calls to lexer.next() at the end of the token stream
         // continue to return the EOF token.
-        assert_eq!(lexer.next(), Token::without_location(TokenType::EOF, ""));
+        assert_eq!(lexer.next(), token(TokenType::EOF, ""));
+    }
+
+    #[test]
+    fn operators() {
+        let mut lexer = Lexer::new("<string>", "+-*/%");
+        assert_eq!(lexer.token(), token(TokenType::Plus, "+"));
+        assert_eq!(lexer.next(), token(TokenType::Minus, "-"));
+        assert_eq!(lexer.next(), token(TokenType::Star, "*"));
+        assert_eq!(lexer.next(), token(TokenType::Slash, "/"));
+        assert_eq!(lexer.next(), token(TokenType::Percent, "%"));
+        assert_eq!(lexer.next(), token(TokenType::EOF, ""));
+    }
+
+    #[test]
+    fn symbols() {
+        let mut lexer = Lexer::new("<string>", "_ abc0 lorem_ipsum");
+        assert_eq!(lexer.token(), token(TokenType::Symbol, "_"));
+        assert_eq!(lexer.next(), token(TokenType::Symbol, "abc0"));
+        assert_eq!(lexer.next(), token(TokenType::Symbol, "lorem_ipsum"));
+    }
+
+    #[test]
+    fn keywords() {
+        let mut lexer = Lexer::new(
+            "<string>",
+            "let assert record new and or not if else while for in const func",
+        );
+        assert_eq!(lexer.token(), token(TokenType::Let, "let"));
+        assert_eq!(lexer.next(), token(TokenType::Assert, "assert"));
+        assert_eq!(lexer.next(), token(TokenType::Record, "record"));
+        assert_eq!(lexer.next(), token(TokenType::New, "new"));
+        assert_eq!(lexer.next(), token(TokenType::And, "and"));
+        assert_eq!(lexer.next(), token(TokenType::Or, "or"));
+        assert_eq!(lexer.next(), token(TokenType::Not, "not"));
+        assert_eq!(lexer.next(), token(TokenType::If, "if"));
+        assert_eq!(lexer.next(), token(TokenType::Else, "else"));
+        assert_eq!(lexer.next(), token(TokenType::While, "while"));
+        assert_eq!(lexer.next(), token(TokenType::For, "for"));
+        assert_eq!(lexer.next(), token(TokenType::In, "in"));
+        assert_eq!(lexer.next(), token(TokenType::Const, "const"));
+        assert_eq!(lexer.next(), token(TokenType::Func, "func"));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate lazy_static;
+
 mod analyzer;
 mod ast;
 mod codegen;
