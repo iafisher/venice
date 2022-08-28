5f208afba4aa489005380ca94db89a3f22868ae7
Ian Fisher <ian@iafisher.com>
2022-08-28 12:02:56 -0400

    No need for separate folders for each test program

--- tests/end_to_end.rs
+++ tests/end_to_end.rs
@@ -1,6 +1,12 @@
 // Copyright 2022 The Venice Authors. All rights reserved.
 // Use of this source code is governed by an MIT-style license that can be
 // found in the LICENSE file.
+//
+// End-to-end tests for the Venice compiler.
+//
+// Each test executes the compiler on a Venice program and checks the output (both stdout and
+// stderr) and intermediate representations (VIL and x86 assembly) against a snapshot. Snapshotting
+// is handled by the `insta` crate.
 
 use std::fs;
 use std::fs::File;
@@ -62,16 +68,16 @@ fn test_13_argv() {
     test_e2e_with_args("13_argv", &["a", "b", "c"]);
 }
 
-fn test_e2e(folder: &str) {
-    test_e2e_with_args(folder, &[]);
+fn test_e2e(base_name: &str) {
+    test_e2e_with_args(base_name, &[]);
 }
 
-fn test_e2e_with_args(folder: &str, args: &[&str]) {
-    let bin_path = build_path(folder, "program");
-    let obj_path = build_path(folder, "program.o");
-    let vil_path = build_path(folder, "program.vil");
-    let x86_path = build_path(folder, "program.x86.s");
-    let input_path = build_path(folder, "program.vn");
+fn test_e2e_with_args(base_name: &str, args: &[&str]) {
+    let bin_path = build_path(base_name, "");
+    let obj_path = build_path(base_name, "o");
+    let vil_path = build_path(base_name, "vil");
+    let x86_path = build_path(base_name, "x86.s");
+    let input_path = build_path(base_name, "vn");
 
     // Ensure that intermediate files are removed at the end of the test.
     let _cleanup = CleanupFile(vec![
@@ -95,9 +101,9 @@ fn test_e2e_with_args(folder: &str, args: &[&str]) {
 
     // Check the intermediate files.
     let vil_output = read_file(&vil_path);
-    insta::assert_display_snapshot!(format!("{}-vil", folder), vil_output);
+    insta::assert_display_snapshot!(format!("{}-vil", base_name), vil_output);
     let x86_output = read_file(&x86_path);
-    insta::assert_display_snapshot!(format!("{}-x86", folder), x86_output);
+    insta::assert_display_snapshot!(format!("{}-x86", base_name), x86_output);
 
     // Run the binary itself, under the `timeout` utility so it doesn't run forever.
     let output = Command::new("timeout")
@@ -110,9 +116,9 @@ fn test_e2e_with_args(folder: &str, args: &[&str]) {
 
     // Check the output.
     let stdout = str::from_utf8(&output.stdout).unwrap();
-    insta::assert_display_snapshot!(format!("{}-stdout", folder), stdout);
+    insta::assert_display_snapshot!(format!("{}-stdout", base_name), stdout);
     let stderr = str::from_utf8(&output.stderr).unwrap();
-    insta::assert_display_snapshot!(format!("{}-stderr", folder), stderr);
+    insta::assert_display_snapshot!(format!("{}-stderr", base_name), stderr);
 }
 
 struct CleanupFile(Vec<String>);
@@ -125,11 +131,11 @@ impl Drop for CleanupFile {
     }
 }
 
-fn build_path(folder: &str, file: &str) -> String {
+fn build_path(base_name: &str, ext: &str) -> String {
     let mut buf = PathBuf::new();
     buf.push("tests");
-    buf.push(folder);
-    buf.push(file);
+    buf.push(base_name);
+    buf.set_extension(ext);
     buf.into_os_string().into_string().unwrap()
 }
 
